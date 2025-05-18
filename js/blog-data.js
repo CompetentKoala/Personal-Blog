7a6c5c741a3299da65bedc9210455e0118961ec3
Cameron <cameronkuy@gmail.com>
2025-05-17 18:43:10 -0700

    Blog post 2: Replika Bot

--- js/blog-data.js
+++ js/blog-data.js
@@ -3,8 +3,12 @@ const blogData = [
     title: "Why I’m Documenting My AI Journey",
     text: "The why's and how's of acheiving my goals",
     img: "/images/camface.png",
-    url: "/blog-posts/post1.html"
+    url: "/blog-posts/post1.html",
   },
-
-
+  {
+    title: "Replika AI Chat Bot",
+    text: "The why's and how's of acheiving my goals",
+    img: "/images/replica.png",
+    url: "/blog-posts/post2.html",
+  },
 ];
