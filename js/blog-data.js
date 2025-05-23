22b0a06e798391845c4bdbf5564c137030c5f7b5
Cameron <cameronkuy@gmail.com>
2025-05-23 16:39:42 -0700

    Added new post: Post 3

--- js/blog-data.js
+++ js/blog-data.js
@@ -3,12 +3,18 @@ const blogData = [
     title: "Why I’m Documenting My AI Journey",
     text: "The why's and how's of acheiving my goals",
     img: "/images/camface.png",
-    url: "/blog-posts/1.Why I’m Documenting My AI Journey.html",
+    url: "/blog-posts/1-Why-I’m-Documenting-My-AI-Journey.html",
   },
   {
     title: "Replika AI Chat Bot",
     text: "Can, and SHOULD AI replace friendships and human connection? ",
     img: "/images/replica.png",
-    url: "/blog-posts/post2.html",
+    url: "/blog-posts/2-Replika-AI-Chat-Bot.html",
+  },
+  {
+    title: "The Truth of The Environmental Impact of AI",
+    text: "The Planet’s Strain Isn’t Just From AI, It’s From 8 Billion Mouths.",
+    img: "/images/3.main.jpg",
+    url: "/blog-posts/3-The-Truth-of-The-Environmental-Impact-of-AI.html",
   },
 ];
