4af8c188090ba098b59acb52ba6d3de2128eb7a2
Cameron <cameronkuy@gmail.com>
2025-05-28 11:57:10 -0700

    new post 4- Writing of Data engineering

--- js/blog-data.js
+++ js/blog-data.js
@@ -12,9 +12,15 @@ const blogData = [
     url: "/blog-posts/2-Replika-AI-Chat-Bot.html",
   },
   {
-    title: "The Truth of The Environmental Impact of AI",
-    text: "The Planet’s Strain Isn’t Just From AI, It’s From 8 Billion Mouths.",
+    title: "AI vs Beef: What’s Really Draining the Planet?",
+    text: "This article compares the water usage of AI and the meat industry, revealing how normalized habits like beef consumption quietly overshadow the environmental impact of newer technologies.",
     img: "/images/3.main.jpg",
     url: "/blog-posts/3-The-Truth-of-The-Environmental-Impact-of-AI.html",
+  },
+  {
+    title: "The Wonderful World of Data and Its Writing Methods",
+    text: "Exploring the often-overlooked writing responsibilities in data engineering and AI, from technical design documents to the critical work of AI ethics researchers.",
+    img: "/images/3.main.jpg",
+    url: "/blog-posts/4-Writing-Methods-Data-Engineers.html",
   },
 ];
