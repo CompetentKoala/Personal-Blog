a09bc70e36ee1b904c420e0ebb54e92c3957a98b
Cameron <cameronkuy@gmail.com>
2025-05-28 12:05:29 -0700

    updates post image

--- js/blog-data.js
+++ js/blog-data.js
@@ -13,14 +13,14 @@ const blogData = [
   },
   {
     title: "AI vs Beef: What’s Really Draining the Planet?",
-    text: "This article compares the water usage of AI and the meat industry, revealing how normalized habits like beef consumption quietly overshadow the environmental impact of newer technologies.",
+    text: "Comparing the water usage of AI and the meat industry, revealing how normalized habits like beef consumption quietly overshadow the environmental impact of newer technologies.",
     img: "/images/3.main.jpg",
     url: "/blog-posts/3-The-Truth-of-The-Environmental-Impact-of-AI.html",
   },
   {
     title: "The Wonderful World of Data and Its Writing Methods",
     text: "Exploring the often-overlooked writing responsibilities in data engineering and AI, from technical design documents to the critical work of AI ethics researchers.",
-    img: "/images/3.main.jpg",
+    img: "/images/4-writing.jpg",
     url: "/blog-posts/4-Writing-Methods-Data-Engineers.html",
   },
 ];
